8135fddf7082e15de31bb5a0ce805724a6b0aa98
Julienne Andrea Panes <juliennepanes@gmail.com>
2026-07-08 04:40:47 +0800

    Fix isAuthenticated always being truthy on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import LoginButton from "./components/auth/LoginButton";
 import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import DriveSection from "./components/drive/DriveSection";
 
-export default function Home() {
-  const session = getServerSession();
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   const isAuthenticated = !!session;
 
   return (
