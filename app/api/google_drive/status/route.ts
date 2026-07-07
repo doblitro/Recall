95825d1731b109e4a9a6cbc27d243328092743a8
Julienne Andrea Panes <juliennepanes@gmail.com>
2026-07-08 03:35:10 +0800

    Remove legacy Google Drive-specific connect routes and components

--- app/api/google_drive/status/route.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-
-export async function GET() {
-  const cookieStore = await cookies();
-  const isDriveConnected = !!cookieStore.get("drive_access_token")?.value;
-
-  return Response.json({ isDriveConnected });
-}
