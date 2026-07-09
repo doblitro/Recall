8545086f3c85678486cec1a9c174ad375eac8189
Julienne Andrea Panes <juliennepanes@gmail.com>
2026-07-09 23:53:05 +0800

    Add initial metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,70 @@ const geist = Geist({
   display: "swap",
 });
 
+const title = "Recall";
+const description = "Search everything you've ever connected.";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://recall.juliennepanes.workers.dev"),
+  title: {
+    default: title,
+    template: `%s · ${title}`,
+  },
+
+  description,
+
+  applicationName: title,
+
+  icons: {
+    icon: "/icon.svg",
+  },
+
+  authors: [
+    {
+      name: "Julienne Panes",
+    },
+  ],
+
+  creator: "Julienne Panes",
+
+  keywords: [
+    title,
+    "Personal Search Engine",
+    "Search",
+    "Knowledge Management",
+    "Google Drive",
+    "Gmail",
+    "GitHub",
+    "Notion",
+    "Productivity",
+    "Full-Text Search",
+  ],
+
+  openGraph: {
+    title: title,
+    description,
+    url: "https://recall.juliennepanes.workers.dev",
+    siteName: title,
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: title,
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description,
+    images: ["/preview.png"],
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
