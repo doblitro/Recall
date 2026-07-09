b5df2c21e3b30329f2c06c8a6845f2cafdb05f7a
Julienne Andrea Panes <juliennepanes@gmail.com>
2026-07-09 23:25:52 +0800

    Update styles variables and add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
 import Providers from "./providers/Providers";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">
+      <body className={`min-h-full flex flex-col ${geist.className}`}>
         <Providers>{children}</Providers>
       </body>
     </html>
