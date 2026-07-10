76c55786543ef75f2f56fad53e99ab30a168f90d
Julienne Andrea Panes <juliennepanes@gmail.com>
2026-07-10 23:05:31 +0800

    Fix sidebar mobile height

--- app/components/ui/Sidebar.tsx
+++ app/components/ui/Sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar = () => {
         )}
       </button>
       <aside
-        className={`fixed md:sticky top-0 left-0 h-screen flex flex-col justify-between bg-accent-foreground border-r border-border shadow-sm transition-all duration-200 overflow-hidden z-40 md:z-2 ${
+        className={`fixed md:sticky top-0 left-0 h-full flex flex-col justify-between bg-accent-foreground border-r border-border shadow-sm transition-all duration-200 overflow-hidden z-40 md:z-2 ${
           isOpen
             ? "w-64 p-4 translate-x-0"
             : "w-64 p-4 -translate-x-full md:w-0 md:p-0 md:border-r-0 md:translate-x-0"
